5e68aa59af562a26e307aed1ba4cc9eb328a8c0f
mstearne <mstearne@Michaels-Mac-mini.local>
2015-02-11 18:17:56 -0500

    updates

--- shared.php
+++ shared.php
@@ -23,7 +23,7 @@ if(!$_SESSION['lpusername']&&$_SERVER["SCRIPT_NAME"]!="/liquidplanner/lpLogin.ph
 
 
 if($_SESSION['lpuserid']){
-    $email = $_SESSION['lpusername']; 
+    $email = $_SESSION['lpusername'];  
     $password = $_SESSION['lppassword']; 
     $member_id = $_SESSION['lpuserid']; 
     $lp = new LiquidPlanner($email, $password);
@@ -32,7 +32,11 @@ if($_SESSION['lpuserid']){
     if($_SERVER['REQUEST_URI']=="/liquidplanner/getComments.php"||$_SERVER['REQUEST_URI']=="/liquidplanner/getUnsubmittedTime.php"){
 		scriptTimer(print_r($workspaces,true));	    
     }
-    //print_r($workspaces);
+    if($workspaces->error=="Throttled"){
+	    print "Throttled. Try again or <a href='mailto:mstearne@pathinteractive.com?subject=LP Throttled'>contact support</a>.";
+	    exit();
+    }
+//    print_r($workspaces);
     $ws = $workspaces[0];
     $lp->workspace_id = $ws->id;
     $projects = $lp->projects();
@@ -177,6 +181,51 @@ function scriptTimer($label=""){
 	file_put_contents("tmp/scriptTimer.txt", date("H:i:s").": ".$label." ".$_SERVER["REQUEST_URI"]."\n",FILE_APPEND);
 }
 
-
+function print_r_reverse($in) { 
+    $lines = explode("\n", trim($in)); 
+    if (trim($lines[0]) != 'Array') { 
+        // bottomed out to something that isn't an array 
+        return $in; 
+    } else { 
+        // this is an array, lets parse it 
+        if (preg_match("/(\s{5,})\(/", $lines[1], $match)) { 
+            // this is a tested array/recursive call to this function 
+            // take a set of spaces off the beginning 
+            $spaces = $match[1]; 
+            $spaces_length = strlen($spaces); 
+            $lines_total = count($lines); 
+            for ($i = 0; $i < $lines_total; $i++) { 
+                if (substr($lines[$i], 0, $spaces_length) == $spaces) { 
+                    $lines[$i] = substr($lines[$i], $spaces_length); 
+                } 
+            } 
+        } 
+        array_shift($lines); // Array 
+        array_shift($lines); // ( 
+        array_pop($lines); // ) 
+        $in = implode("\n", $lines); 
+        // make sure we only match stuff with 4 preceding spaces (stuff for this array and not a nested one) 
+        preg_match_all("/^\s{4}\[(.+?)\] \=\> /m", $in, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER); 
+        $pos = array(); 
+        $previous_key = ''; 
+        $in_length = strlen($in); 
+        // store the following in $pos: 
+        // array with key = key of the parsed array's item 
+        // value = array(start position in $in, $end position in $in) 
+        foreach ($matches as $match) { 
+            $key = $match[1][0]; 
+            $start = $match[0][1] + strlen($match[0][0]); 
+            $pos[$key] = array($start, $in_length); 
+            if ($previous_key != '') $pos[$previous_key][1] = $match[0][1] - 1; 
+            $previous_key = $key; 
+        } 
+        $ret = array(); 
+        foreach ($pos as $key => $where) { 
+            // recursively see if the parsed out value is an array too 
+            $ret[$key] = print_r_reverse(substr($in, $where[0], $where[1] - $where[0])); 
+        } 
+        return $ret; 
+    } 
+} 
 
 ?>
